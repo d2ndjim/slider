6eeabb77f6285c06c93786742a0a945a56d8b399
Lekan <jimoridwanlekan@gmail.com>
2022-06-12 20:04:39 +0100

    check and fix linters errors

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,53 +1,56 @@
-import React, {useState, useEffect} from "react";
-import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
-import { FaQuoteRight } from "react-icons/fa";
-import data from './data'
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 
 const Slider = () => {
-  const [people, setPeople] = useState(data)
-  const [index, setIndex] = useState(0)
+  const people = data;
+  const [index, setIndex] = useState(0);
 
   const handleNext = () => {
-    setIndex((prev) => prev + 1)
+    setIndex((prev) => prev + 1);
   };
 
   const handlePrev = () => {
-    setIndex((prev) => prev - 1)
+    setIndex((prev) => prev - 1);
   };
 
   useEffect(() => {
-    const lastIndex = people.length - 1
+    const lastIndex = people.length - 1;
     if (index < 0) {
-      setIndex(lastIndex)
+      setIndex(lastIndex);
     }
     if (index > lastIndex) {
-      setIndex(0)
+      setIndex(0);
     }
   }, [index, people]);
 
   useEffect(() => {
-    let slider = setInterval(() => {
-      setIndex(index + 1)
-    }, 7000)
-    return () => clearInterval(slider)
-  }, [index])
+    const slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 7000);
+    return () => clearInterval(slider);
+  }, [index]);
 
   return (
     <section className="section">
       <div className="title">
         <h2>
-          <span>/</span>Reviews
+          <span>/</span>
+          Reviews
         </h2>
       </div>
       <div className="section-center">
         {people.map((person, personIndex) => {
-          const { id, image, name, title, quote } = person;
-          let position = 'nextSlide'
+          const {
+            id, image, name, title, quote,
+          } = person;
+          let position = 'nextSlide';
           if (personIndex === index) {
-            position = 'activeSlide'
+            position = 'activeSlide';
           }
-          if (personIndex === - 1 || (index === 0 && personIndex === people.length - 1)) {
-            position = 'lastSlide'
+          if (personIndex === -1 || (index === 0 && personIndex === people.length - 1)) {
+            position = 'lastSlide';
           }
           return (
             <article className={position} key={id}>
@@ -59,15 +62,15 @@ const Slider = () => {
             </article>
           );
         })}
-        <button className="prev" onClick={handlePrev}>
+        <button type="button" className="prev" onClick={handlePrev}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={handleNext}>
+        <button type="button" className="next" onClick={handleNext}>
           <FiChevronRight />
         </button>
       </div>
     </section>
   );
-}
+};
 
-export default Slider
+export default Slider;
